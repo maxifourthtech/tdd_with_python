818d58f2f564459590b0bd23ac29115abe9f00fb
Maxi Berton Albornoz <maxi@fourthtech.ai>
2023-07-23 08:26:23 -0300

    First FT specked out in comments, and now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -3,12 +3,18 @@ from selenium import webdriver
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
     def test_something(self):
         browser = webdriver.Chrome()
         browser.get('http://127.0.0.1:8000/')
 
-        self.assertTrue('Django' in browser.title)
+        self.assertTrue('To-Do' in browser.title)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(warnings='ignore')
