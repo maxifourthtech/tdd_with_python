5e027dcc6dcc225ab3c9b933996db8824b2a045f
Maxi Berton Albornoz <maxi@fourthtech.ai>
2023-07-23 09:58:23 -0300

    Functional test now checks we can input a to-do item.

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
+import time
 import unittest
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
 class MyTestCase(unittest.TestCase):
@@ -10,10 +13,39 @@ class MyTestCase(unittest.TestCase):
         self.browser.quit()
 
     def test_something(self):
-        browser = webdriver.Chrome()
-        browser.get('http://127.0.0.1:8000/')
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://127.0.0.1:8000/')
 
-        self.assertTrue('To-Do' in browser.title)
+        # She notices the page title and header mention to-do lists
+        self.assertTrue('To-Do' in self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do lists
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+                        'New to-do item did not appear in table')
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        self.fail('Finish the test!')
+
+        # The page updates again, and now shows both items on her lists
 
 
 if __name__ == '__main__':
